f51ef9389b969346b0c4734c85022974664ec053
tahz <tahdn4@gmail.com>
2020-05-27 06:59:22 +0700

    27/5/20

---
+++ Controllers/Movie.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Workshop1.Models;
+namespace Workshop1.Controllers
+{
+    public class Movie :Controller
+    {
+        public string index(){
+            return "my name tahz";
+        }
+
+
+        
+    }
+}
